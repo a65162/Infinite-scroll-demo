ab0ffba5206fa6a5ffac963876ef34a2c1307109
Wei <git@a-wei.link>
2026-03-09 18:01:21 +0800

    chore(eslint): add some rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ export default withNuxt([
   {
     rules: {
       'no-console': ['error'],
+      'vue/no-multiple-template-root': 'off',
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     },
   },
 ])
