0b9f5e84d57d1c83d467d9fcc50d316190f4a98a
Wei <git@a-wei.link>
2026-03-09 09:50:27 +0800

    chore(eslint): enable no-console rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,10 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt([
+  {
+    rules: {
+      'no-console': ['error'],
+    },
+  },
+])
